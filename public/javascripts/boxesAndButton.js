4116f0552689233bda69afcb812e8ed264cc636d
Morgan Chafe <mc.github@outlook.com>
2021-10-13 04:11:26 -0230

    First iteration of full checkbox functionality

--- public/javascripts/boxesAndButton.js
+++ public/javascripts/boxesAndButton.js
@@ -14,7 +14,12 @@ function setButtonState(checkedBoxes)
     } 
 }
 
-checkBoxes.forEach(checkBox => {   
+function getTabDepth(checkBox)
+{
+    return parseInt(checkBox.nextElementSibling.classList[1].slice(3));
+}
+
+checkBoxes.forEach((checkBox,idx) => {   
     if(checkBox.checked)
     {
         checkedBoxes+=1;
@@ -22,6 +27,33 @@ checkBoxes.forEach(checkBox => {
     checkBox.addEventListener("change",()=>{
         if(checkBox.checked)
         {
+            //TODO: when you click on a parent ingredient, all the children ingredient should also be checked.
+            //Idea: suppose a tab3 ingredient was clicked. then, check all boxes up to the next tab3 ingredient, etc.  
+            const tabDepth = getTabDepth(checkBox);
+            console.log("tabDepth = ", tabDepth);
+            for(let i = idx; i<checkBoxes.length-1; i++)
+            {
+                const depth = getTabDepth(checkBoxes[i+1]);
+                console.log("depth: ",depth)
+                if(depth !== tabDepth)
+                {
+                    console.log("depth !== tabDepth", depth !== tabDepth)
+                    const label = document.querySelector(`label[for='${checkBoxes[i+1].id}']`);
+                    console.log("label.innerText",label.innerText );                  
+
+                    checkBoxes[i+1].checked = true;
+                    label.classList.toggle("text-muted");
+                    checkedBoxes+=1;
+                    setButtonState(checkedBoxes)
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+
             const label = document.querySelector(`label[for='${checkBox.id}']`);
             label.classList.toggle("text-muted");
             checkedBoxes+=1;
@@ -39,8 +71,7 @@ checkBoxes.forEach(checkBox => {
 })
 
 button.addEventListener("click",()=>{
-    // const checkBoxes = document.querySelectorAll("input[type='checkbox']")
-    checkBoxes.forEach(checkBox =>{
+    checkBoxes.forEach((checkBox,idx) =>{
         if(checkBox.checked)
         {
             const label = document.querySelector(`label[for='${checkBox.id}']`);
@@ -53,5 +84,3 @@ button.addEventListener("click",()=>{
 })
 
 
-//TODO: when you click on a parent ingredient, all the children ingredient should also be checked.
-//Idea: suppose a tab3 ingredient was clicked. then, check all boxes up to the next tab3 ingredient, etc.
