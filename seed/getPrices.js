d899e86020cedb30e50463f82a77ad7a6868a5a4
Morgan Chafe <mc.github@outlook.com>
2021-10-13 11:12:23 -0230

    Update getPrices.js

--- seed/getPrices.js
+++ seed/getPrices.js
@@ -5,24 +5,39 @@ const {getItemInfo} = require("../utils/potionDataHelpers");
 const runescape = require("runescape-api");
 const grandexchange = runescape.grandexchange;
 
-
 const untradeableItems = [
+    12158,
+    12159,
+    12160,
+    12163,
+    1525,
     1526,
-    899995,
-    899996,
-    899997,
-    899998,
-    899999,
-    900000,
-    900001,
-    900002,
-    900003,
-    900004,
-    900005,
-    900006,
-    900007,
-    900008,
-    49123,
+    1526,
+    1533,
+    1534,
+    5004,
+    19972,
+    19973,
+    19974,
+    19975,
+    19976,
+    19977,
+    19978,
+    19979,
+    19980,
+    19981,
+    19982,
+    19983,
+    19984,
+    19985,
+    19986,
+    19987,
+    19988,
+    19989,
+    19990,
+    19991,
+    19992,
+    19993,
     19972,
     19973,
     19975,
@@ -38,12 +53,8 @@ const untradeableItems = [
     19986,
     19987,
     19988,
-    1525,
-    1533,
-    1534,
-    1526,
-    19990,
     19989,
+    19990,
     19991,
     19992,
     19993,
@@ -52,10 +63,21 @@ const untradeableItems = [
     20000,
     20001,
     20002,
-    12158,
-    12159,
-    12160,
-    12163,
+    49123,
+    899995,
+    899996,
+    899997,
+    899998,
+    899999,
+    900000,
+    900001,
+    900002,
+    900003,
+    900004,
+    900005,
+    900006,
+    900007,
+    900008,
     900009,
     900010,
     900012,
@@ -66,9 +88,6 @@ const untradeableItems = [
     900017,
     900018
 ]
-
-
-
 
 mongoose.connect(dbUrl)
     .then(()=>{
@@ -89,7 +108,7 @@ async function fetchPrices()
             if(!untradeableItems.find(id => id === item.itemID) && item.dailyPrice === 0)
             {
                 console.log(item.name);
-                await new Promise(resolve => setTimeout(resolve, 2000)) 
+                await new Promise(resolve => setTimeout(resolve, 2500)) 
                 const graph = await grandexchange.getItemGraph(parseInt(item.itemID));
                 const exactPrice = graph.daily[Object.keys(graph.daily)[Object.keys(graph.daily).length-1]]
                 console.log(exactPrice)
