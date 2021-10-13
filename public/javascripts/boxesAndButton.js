b4e5db685525f8ac4883792bffe2c5968832cc63
Morgan Chafe <mc.github@outlook.com>
2021-10-13 05:41:21 -0230

    Fixed some checkbox issues. Need to add the 'reverse' of it.

--- public/javascripts/boxesAndButton.js
+++ public/javascripts/boxesAndButton.js
@@ -2,6 +2,21 @@ const checkBoxes = document.querySelectorAll("input[type='checkbox']")
 const button = document.querySelector("#uncheck");
 let checkedBoxes = 0;
 
+//clear all selected checkboxes and text-muted classes
+window.onload = function() {
+    for(const checkBox of checkBoxes)
+    {
+        checkBox.checked = false;
+        const label = document.querySelector(`label[for='${checkBox.id}']`);
+        if(label)
+        {
+            label.classList.remove("text-muted"); 
+        }
+    }
+    checkedBoxes=0;
+    button.disabled=true;
+  };
+
 function setButtonState(checkedBoxes)
 {
     if(checkedBoxes===0)
@@ -37,31 +52,40 @@ checkBoxes.forEach((checkBox,idx) => {
     checkBox.addEventListener("change",()=>{
         if(checkBox.checked)
         {
-            //TODO: when you click on a parent ingredient, all the children ingredient should also be checked.
-            //Idea: suppose a tab3 ingredient was clicked. then, check all boxes up to the next tab3 ingredient, etc.  
+            //handle the initially clicked box
+            console.log(checkBox.id);
+            const label = document.querySelector(`label[for='${checkBox.id}']`);
+            label.classList.toggle("text-muted")
+            checkedBoxes+=1;
+            setButtonState(checkedBoxes)
+            
             let tabDepth = getTabDepth(checkBox);
             if(tabDepth)
             {            
-                console.log("tabDepth = ", tabDepth);
+                //handle all the 'children' boxes
                 for(let i = idx; i<checkBoxes.length-1; i++)
                 {
+                    if(checkBoxes[i+1] === checkBoxes[checkBoxes.length-1])
+                    {
+                        const label = document.querySelector(`label[for='${checkBoxes[i+1].id}']`);
+                        label.classList.toggle("text-muted");
+                        checkBoxes[i+1].checked = true;
+                        checkedBoxes+=1;
+                        setButtonState(checkedBoxes)
+                    }
+                    
                     const depth = getTabDepth(checkBoxes[i+1]);
-                    console.log("depth: ",depth)
                     if(checkBoxes[i+1].checked === true)
                     {
                         break;
                     }
                     if(depth > tabDepth)
                     {
-                        console.log("depth !== tabDepth", depth !== tabDepth)
                         const label = document.querySelector(`label[for='${checkBoxes[i+1].id}']`);
-                        console.log("label.innerText",label.innerText );                  
-
-                        checkBoxes[i+1].checked = true;
                         label.classList.toggle("text-muted");
+                        checkBoxes[i+1].checked = true;
                         checkedBoxes+=1;
                         setButtonState(checkedBoxes)
-
                     }
                     else
                     {
@@ -69,11 +93,6 @@ checkBoxes.forEach((checkBox,idx) => {
                     }
                 }
             }
-
-            const label = document.querySelector(`label[for='${checkBox.id}']`);
-            label.classList.toggle("text-muted");
-            checkedBoxes+=1;
-            setButtonState(checkedBoxes)
         }
         else
         {
@@ -86,6 +105,7 @@ checkBoxes.forEach((checkBox,idx) => {
     setButtonState(checkedBoxes)
 })
 
+//clear all selected checkboxes and text-muted classes
 button.addEventListener("click",()=>{
     checkBoxes.forEach((checkBox,idx) =>{
         if(checkBox.checked)
