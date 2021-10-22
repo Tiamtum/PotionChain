af6b72c8ef811d95e73b7665599d55340b8e3c96
Morgan Chafe <mc.github@outlook.com>
2021-10-22 15:52:22 -0230

    Worked on untradable prices.

--- controllers/index.js
+++ controllers/index.js
@@ -30,7 +30,6 @@ module.exports.renderChangelog = (req,res)=>{
 }
 
 module.exports.showResults = async (req,res)=>{  
-    console.log("req.query", req.query);
     try
     {
         const name = await parseName(req.query.name.trim());
@@ -51,33 +50,77 @@ module.exports.showResults = async (req,res)=>{
             const coinPile = getCoinPile(totalPrice.value);
             ingredient.data = {number,image,exactPrice,totalPrice,coinPile};
             req.session.data.push(ingredient);
-            console.log(`${ingredient.item} RECIEVED`,`itemID: ${ingredient.itemID}`);
+            // console.log(`${ingredient.item} RECIEVED`,`itemID: ${ingredient.itemID}`);
         }
         
-        
-        // const finalPrice = getFinalPrice(ingredients,display);
-        // console.log("finalPrice",finalPrice);
         let totals = 0 ;
+        console.log("ingredients.length=",ingredients.length)
         for(let i = 1; i<ingredients.length; i++)
         {
-            totals += ingredients[i].data.totalPrice.value 
-        }
-        // const cost = ingredients[0].data.totalPrice.value - totals;
-        // const priceToMake = {"value":totals,"string":parsePrice(totals),"coinPile":getCoinPile(totals)}
-        // const profit = {"value":cost, "string":parsePrice(cost),"coinPile":getCoinPile(cost)};
+            if(ingredients[i].tab === 1 && ingredients[i].data.totalPrice.value !== 0)
+            {
+                console.log("ADDING",ingredients[i].item,"'s price to total");
+                totals += ingredients[i].data.totalPrice.value 
+            }
+            else if(ingredients[i].tab === 1 && ingredients[i].data.totalPrice.value === 0)
+            {
+                // console.log(ingredients[i].item," is value 0");
+                for(let j = i; j<ingredients.length; j++)
+                {
+                    // console.log("let j = i; j<ingredients.length; j++")
+                    if(ingredients[j+1].tab === 1)
+                    {
+                        // totals += ingredients[j].data.totalPrice.value;
+                        console.log("break reached on item: ",ingredients[j].item )
+                        break;
+                    }
+                    else
+                    {
+                        if(ingredients[j].tab === 2)
+                        {
+                            console.log("ADDING ",ingredients[j].item,"'s price to total");
+                            totals += ingredients[j].data.totalPrice.value;
+                        }
+                    }
+                }
+            }
 
+            // if(ingredients[i].tab === 1 && ingredients[i].data.totalPrice.value === 0 )
+            // {
+            //     for(let j = i; j<ingredients.length; j++)
+            //     {
+            //         // console.log("let j = i; j<ingredients.length; j++")
+            //         if(ingredients[j+1].tab === 1)
+            //         {
+            //             // totals += ingredients[j].data.totalPrice.value;
+            //             console.log("break reached on item: ",ingredients[j].item )
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             if(ingredients[j].tab === 2)
+            //             {
+            //                 totals += ingredients[j].data.totalPrice.value;
+            //             }
+            //         }
+            //     }
+            // }
+        }
+        console.log("totals = ",totals)
+        console.log(ingredients[0].item,ingredients[0].data.totalPrice)
         const costData = 
         {
             "profit" : {"value":ingredients[0].data.totalPrice.value - totals, "string":parsePrice(ingredients[0].data.totalPrice.value - totals),"coinPile":getCoinPile(ingredients[0].data.totalPrice.value - totals)},
             "priceToMake" : {"value":totals,"string":parsePrice(totals),"coinPile":getCoinPile(totals)}
         }
+        console.log("costData",costData);
+        // console.log(ingredients);
+        console.log(ingredients[1].item,ingredients[1].data.totalPrice)
 
-        // req.session.profit = profit;
-        // req.session.finalPrice = finalPrice;
         req.session.costData = costData;
         req.session.displaySetting = display;
         req.session.save();
-        console.log(ingredients);
+
         res.render("results",{ingredients,costData,"displaySetting":display,pageTitle:"Potion Chain"}) 
 
     }
